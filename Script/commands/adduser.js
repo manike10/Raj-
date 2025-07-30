4cc53b02d4280dd1e55601981c35d42ee6cc912b
manike10 <mdmusa149194@gmail.com>
2025-07-31 03:31:17 +0600

    Update adduser.js

--- Script/commands/adduser.js
+++ Script/commands/adduser.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adduser",
 	version: "2.4.3",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "‎⤾➝অৃঁপ্রিৃঁয়ৃঁ ↬রাৃঁজৃঁ⤹❤️‍🩹🤍🙂⇣",
 	description: "Add user to the group by link or id",
 	commandCategory: "group",
 	usages: "[args]",
