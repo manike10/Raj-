cd458ca3495803d18fb0b0701487aba9badeb298
manike10 <mdmusa149194@gmail.com>
2025-07-31 04:13:22 +0600

    Update Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "‎⤾➝অৃঁপ্রিৃঁয়ৃঁ↬রাৃঁজৃঁ⤹❤️‍🩹🤍🙂⇣",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
